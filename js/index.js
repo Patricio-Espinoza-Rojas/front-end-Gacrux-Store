cdfabc71d037d1dc0ea1799949bac7403469d8f6
Patricio Espinoza <p.alfonsoespinoza@gmail.com>
2023-05-26 19:07:00 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 let navbar = document.querySelector('.navbar');
 
+
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
